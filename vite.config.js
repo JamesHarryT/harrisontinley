9102b485b87e18aee3f008933faa8702bb01a4e8
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2025-05-15 16:43:53 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	// root: '.',            ← default, so no need to include
-	// publicDir: 'public',  ← also default
+	base: 'https://jamesharryt.github.io/harrisontinley/',
 });
 
