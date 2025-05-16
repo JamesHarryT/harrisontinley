b45ebf88a8d03681c12102c21fd53239a5c255d1
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2025-05-15 19:52:43 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import * as THREE from URL('https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js');
-import { OrbitControls } from URL('https://cdn.jsdelivr.net/npm/three@0.150.1/examples/jsm/controls/OrbitControls.js');
-
+import './style.css'; // You added this to import your CSS
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // === Scene Setup ===
 const scene = new THREE.Scene();
