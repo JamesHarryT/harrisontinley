53e8ee85d7e37a6d86369ee6cdb56b6151a25629
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2025-05-15 22:56:31 -0700

    paths

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log("i opened vro");
 import * as THREE from 'https://unpkg.com/three@0.160.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.160.1/examples/jsm/controls/OrbitControls.js';
 
