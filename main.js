cf99ae054fc7a9d1db11d2ea1931747c39cc9a5a
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2025-05-15 23:09:02 -0700

    added three manually

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from './three';
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls';
+import * as THREE from './js/three.module.js';
+import { OrbitControls } from './js/OrbitControls.js';
 console.log("I imported");
 
 // === Scene Setup ===
