1b3ef526a1ab08937b8329504bc48dd4a82354f6
JamesHarryT <140349577+JamesHarryT@users.noreply.github.com>
2025-05-19 21:54:33 -0700

    finished personalization
    added licensing and moved around models

--- main.v2.js
+++ main.v2.js
@@ -28,38 +28,38 @@ function loadModel(path, position = { x: 0, y: 0, z: 0 }, scale = 1, animate = f
 	});
 }
 
-loadModel('./public/assets/models/Donut.glb', { x: -13, y: 0, z: 30 }, 20.0, true);
+loadModel('./public/assets/models/Donut.glb', { x: -10, y: 3, z: 65 }, 20.0, true);
 loadModel('./public/assets/models/meCube.glb', { x: 5, y: 0, z: -10 }, 1.0, true);
 loadModel('./public/assets/models/LowPolyInu1.glb', { x: 5, y: 1, z: 5}, 4.0, true);
-loadModel('./public/assets/models/ChessScene.glb', { x: -15, y: 1, z: 10 }, 4.0, true);
+loadModel('./public/assets/models/ChessScene.glb', { x: 1, y: -2, z: 25 }, 4.0, true);
 
 let astro = null;
 let astroAnimation = false;
 loader.load('./public/assets/models/astro.glb', (gltf) => {
 	astro = gltf.scene;
-	astro.position.set(-10, 0, 30);
+	astro.position.set(-10, 0, 60);
 	astro.scale.set(4.0, 4.0, 4.0);
 	scene.add(astro);
 });
 let computer = null;
 loader.load('./public/assets/models/computer.glb', (gltf) => {
 	computer = gltf.scene;
-	computer.position.set(-70, -60, 50);
+	computer.position.set(-55, -55, 110);
 	computer.scale.set(8.0, 8.0, 8.0);
 	scene.add(computer);
 });
 let car = null;
 loader.load('./public/assets/models/Car001.glb', (gltf) => {
 	car = gltf.scene;
-	car.position.set(-25, -1, 10);
+	car.position.set(-20, -1, 35);
 	car.scale.set(5.0, 5.0, 5.0);
 	scene.add(car);
 });
 let youtube = null;
 loader.load('./public/assets/models/youtubeLogo.glb', (gltf) => {
 	youtube = gltf.scene;
-	youtube.position.set(-30, -10, 35);
-	youtube.rotation.y += THREE.MathUtils.degToRad(45);
+	youtube.position.set(-30, -10, 75);
+	youtube.rotation.y += THREE.MathUtils.degToRad(90);
 	youtube.scale.set(4.0, 4.0, 4.0);
 	scene.add(youtube);
 });
